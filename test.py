3d956eb6d0b999c8d927ae815a86fef4a8cba7c1
haidy61 <ahmedhaidy252@gmail.com>
2022-09-04 12:46:15 +0200

    Task 02: Tests Passed

---
+++ test.py
@@ -0,0 +1,20 @@
+target = __import__("sprints.py")
+MyFunc= target.MyFunc
+import unittest
+import statistics
+from sprints import MyFunc
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        data = [10, 20, 30]
+        result =statistics.mean(data)
+        self.assertEqual(result, 20)
+
+    def test_list_fraction(self):
+        data = [2.2, 3.3, 5.9]
+        result = max(data)
+        self.assertEqual(result, 5.9)
+
+    def test_bad_type(self):
+        data = "Haidy"
+        with self.assertRaises(TypeError):
+            result = MyFunc(data)
